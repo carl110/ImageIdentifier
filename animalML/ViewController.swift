6eba1a3e4e9a14667274af7e16ceb2404b31ecea
Carl Wainwright <carljame7@gmail.com>
2019-12-02 11:38:10 +0000

    Initial Commit

---
+++ animalML/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animalML
+//
+//  Created by Carl Wainwright on 02/12/2019.
+//  Copyright © 2019 Carl Wainwright. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
